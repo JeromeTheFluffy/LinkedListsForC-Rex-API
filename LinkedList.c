c5aa32311773292791ed66be4ea6bdb8a8b91d24
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-04-07 16:01:00 -0600

    Create LinkedList.c
    This is part of the API. everything gets imported in the rexAPI.init() function.

---
+++ LinkedList.c
@@ -0,0 +1,8 @@
+/***********************************************
+| This is the rex API
+| usage: listed in the README.md file (that's why it's there)
+| this program is an attempt to add more data structures to C
+***********************************************/
+
+typedef struct intNode{
+    int 
