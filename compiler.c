d00e1caac6fcf51aaf13247b9d64c1fc911e2010
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-04-08 17:02:51 -0600

    Update compiler.c

--- compiler.c
+++ compiler.c
@@ -1,1 +1,10 @@
+/* this is a simple compiler, and will grow with the developement of this language 
+Compiler will accept .stc files(STructured C)
 
+Syntax:
+
+*/
+
+FILE *inputFilePointer, *outputFilePointer;
+inputFilePointer=fopen("helloWorld.stc", "r");
+outputFilePointer=fopen("program.asm, "w");
