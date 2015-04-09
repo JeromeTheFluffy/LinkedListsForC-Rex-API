b443b5ebb2b86e10ce46a7db9c34abbac3861174
JeromeTheFluffy <noah_gray@phrd.ab.ca>
2015-04-09 10:20:11 -0600

    Update compiler.py

--- compiler.py
+++ compiler.py
@@ -1,1 +1,16 @@
-#this is the compiler""
+#this is the compiler#
+#for linux#
+
+inputFile = open("input.stc", "a+")
+i = 0
+theInput = ""
+while inputFile.read(i) != "" :
+  theInput += inputFile.read(i)
+  i += 1
+
+
+output = open("out.asm", "w")
+output.write(
+while True:
+  if "print" in inputFile:
+    
